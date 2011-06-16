8b06378754346370e860607f1c024dfd0d795ac3
Thiago Rafael Becker <thiago.becker@gmail.com>
2011-06-16 00:47:38 -0300

    Initial commit

---
+++ getbacktowork.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# getbacktowork.sh will warn you after some task is completed, so you can slack off 
+# while the computer does the heavy work.
+# Requirements: zenity (linux)
+# Usage: getbacktowork.sh <command>
+
+($@) && zenity --info --text "Work complete" || zenity --error --text "Work failed"
